b8baa1d1a6290551de2597b9f7cfb8f7c042198f
David Alonso <davidalonso991@gmail.com>
2024-07-14 17:55:15 -0600

    Add(index): create basic server

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.list(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
